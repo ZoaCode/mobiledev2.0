f379d604dd195e25d0e2520d327f5c1779e66b60
ZoaCode <118231696+ZoaCode@users.noreply.github.com>
2023-04-26 13:36:56 +0800

    lab 1

---
+++ kab1_dart_excercise/bin/kab1_dart_excercise.dart
@@ -0,0 +1,60 @@
+import 'package:kab1_dart_excercise/kab1_dart_excercise.dart' as kab1_dart_excercise;
+enum Status{
+  none, running,stopped,paused
+}
+
+
+class Book{
+  String author;
+  String title;
+  String description;
+  Publisher publisher;
+
+  Book({this.author, this.title,this.description,this.publisher});
+
+  Book.fromMap(Map<String, dynamic> data){
+    author = data['author'];
+    title =data['title'];
+    description = data['description'];
+    publisher = data['publisher'];
+  }
+}
+void main() {
+  // print(Status.values);
+  // Status.values.forEach((v)=>print('value:$v,index:${v.index}'));
+  // print('running:${Status.running},${Status.running.index}');
+  // print('running index: ${Status.values[1]}');
+
+  
+  var mjobs = {'name':'Johnny','occupation':'gardener'};
+  print(mjobs);
+
+  var mfruits = {1:'Apple',2:'Orange',3:'Grape'};
+  print(mfruits);
+
+  print('There are ${mfruits.length} elements in the map.');
+
+  mjobs['age'] = '42';
+  print(mjobs);
+
+  mfruits[3] = 'Banana';
+  print(mfruits);
+
+  print(mfruits.containsKey(3));
+  print(mfruits.containsValue('Grape'));
+
+
+  mfruits.forEach((key, value) { 
+    print('{key:$key,value:$value}');
+  });
+
+  for(var key in mfruits.keys){print(key);}
+  for(var value in mfruits.values){print(value);}
+
+  mfruits.remove(1);
+  print(mfruits);
+
+  mjobs.clear();
+
+}
+
---
+++ kab1_dart_excercise/test/kab1_dart_excercise_test.dart
@@ -0,0 +1,8 @@
+import 'package:kab1_dart_excercise/kab1_dart_excercise.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
